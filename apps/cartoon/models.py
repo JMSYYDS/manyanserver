a15cef9918086877e54058a16117b50816f3c8d8
Dot <2080970386@qq.com>
2022-07-30 15:53:41 +0800

    更新

--- apps/cartoon/models.py
+++ apps/cartoon/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+# 全部漫画表
+# class cartoon_all_imgs(models.Model):
+#     id = models.IntegerField()
+#     img = models.BinaryField()
+
